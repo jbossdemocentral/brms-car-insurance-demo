68cbbaf33387a3bdb5ffb6c19876bb019b78fe47
Eric D. Schabell <eric@schabell.org>
2013-05-06 14:33:55 +0200

    Added initial car insurance demo porject setup.

---
+++ README.md
@@ -0,0 +1,39 @@
+JBoss BRMS Car Insurance Demo Quickstart Guide
+===================================================
+
+Demo based on JBoss BRMS products.
+
+Setup and Configuration
+-----------------------
+
+See Quick Start Guide in project as ODT and PDF for details on installation. For those that can't wait:
+
+- see README in 'installs' directory
+
+- add products 
+
+- run 'init.sh' & read output
+
+- read Demo Guide
+
+- start JBoss EAP server
+
+- login to BRM (http://localhost:8080/jboss-brms)
+
+- import repository-export from support dir
+
+- build and deploy project in BRM
+
+- login to Business Central (http://localhost:8080/business-central)
+
+- start process, view JBoss EAP logs for results
+
+Windows users see support/windows/README for installation.
+
+Released versions
+-----------------
+
+See the tagged releases for the following versions of the product:
+
+TODO: - v1.0 is BRMS 5.3.1 deployable, running on JBoss EAP 6.
+
---
+++ init.sh
@@ -0,0 +1,169 @@
+#!/bin/sh
+DEMO="Car Insurance Demo"
+JBOSS_HOME=./target/jboss-eap-6.0
+SERVER_DIR=$JBOSS_HOME/standalone/deployments/
+SERVER_CONF=$JBOSS_HOME/standalone/configuration/
+LIB_DIR=./support/lib
+SRC_DIR=./installs
+EAP=jboss-eap-6.0.1.zip
+BRMS=brms-p-5.3.1.GA-deployable-ee6.zip
+DESIGNER=designer-patched.war
+VERSION=5.3.1
+
+
+clear
+
+echo
+echo "Setting up the JBoss BRMS ${DEMO} environment..."
+echo
+
+# make some checks first before proceeding.
+if [[ -f $SRC_DIR/$EAP || -L $SRC_DIR/$EAP ]]; then
+echo EAP sources are present...
+echo
+else
+echo Need to download $EAP package from the Customer Support Portal
+echo and place it in the $SRC_DIR directory to proceed...
+echo
+exit
+fi
+
+# Create the target directory if it does not already exist.
+if [ ! -x target ]; then
+echo " - creating the target directory..."
+echo
+mkdir target
+else
+echo " - detected target directory, moving on..."
+echo
+fi
+
+# Move the old JBoss instance, if it exists, to the OLD position.
+if [ -x $JBOSS_HOME ]; then
+echo " - existing JBoss Enterprise EAP 6 detected..."
+echo
+echo " - moving existing JBoss Enterprise EAP 6 aside..."
+echo
+rm -rf $JBOSS_HOME.OLD
+mv $JBOSS_HOME $JBOSS_HOME.OLD
+
+# Unzip the JBoss EAP instance.
+echo Unpacking JBoss Enterprise EAP 6...
+echo
+unzip -q -d target $SRC_DIR/$EAP
+else
+# Unzip the JBoss EAP instance.
+echo Unpacking new JBoss Enterprise EAP 6...
+echo
+unzip -q -d target $SRC_DIR/$EAP
+fi
+
+# Unzip the required files from JBoss BRMS Deployable
+echo Unpacking JBoss Enterprise BRMS $VERSION...
+echo
+cd installs
+unzip -q $BRMS
+
+echo " - deploying JBoss Enterprise BRMS Manager WAR..."
+echo
+unzip -q -d ../$SERVER_DIR jboss-brms-manager-ee6.zip
+rm jboss-brms-manager-ee6.zip
+
+echo " - deploying jBPM Console WARs..."
+echo
+unzip -q -d ../$SERVER_DIR jboss-jbpm-console-ee6.zip
+rm jboss-jbpm-console-ee6.zip
+
+unzip -q jboss-jbpm-engine.zip
+echo " - copying jBPM client JARs..."
+echo
+unzip -q -d ../$SERVER_DIR jboss-jbpm-engine.zip lib/netty.jar
+cp -r lib ../$LIB_DIR
+cp jbpm-test-5.3.1.BRMS.jar ../$LIB_DIR
+cp jbpm-human-task-5.3.1.BRMS.jar ../$LIB_DIR
+cp jbpm-persistence-jpa-5.3.1.BRMS.jar ../$LIB_DIR
+cp jbpm-workitems-5.3.1.BRMS.jar ../$LIB_DIR
+rm jboss-jbpm-engine.zip
+rm -rf *.jar modeshape.zip *.RSA lib
+rm jboss-brms-engine.zip
+
+echo Rounding up, setting permissions and copying support files...
+echo
+cd ../
+
+echo Updating to the newest web designer...
+echo
+rm -rf $SERVER_DIR/designer.war/*
+unzip -q support/$DESIGNER -d $SERVER_DIR/designer.war
+
+echo "  - set designer to jboss-brms in profile..."
+echo
+cp support/designer-jbpm.xml $SERVER_DIR/designer.war/profiles/jbpm.xml
+
+echo " - enabling demo accounts logins in brms-users.properties file..."
+echo
+cp support/brms-users.properties $SERVER_CONF
+
+echo " - enabling demo accounts role setup in brms-roles.properties file..."
+echo
+cp support/brms-roles.properties $SERVER_CONF
+
+echo " - adding dodeploy files to deploy all brms components..."
+echo
+touch $SERVER_DIR/business-central-server.war.dodeploy
+touch $SERVER_DIR/business-central.war.dodeploy
+touch $SERVER_DIR/designer.war.dodeploy
+touch $SERVER_DIR/jboss-brms.war.dodeploy
+touch $SERVER_DIR/jbpm-human-task.war.dodeploy
+
+echo " - configuring security authentication, copying updated components.xml file to jboss-brms.war..."
+echo
+cp support/components.xml $SERVER_DIR/jboss-brms.war/WEB-INF/
+
+echo " - configuring deployment timeout extention and added security domain brms in standalone.xml..."
+echo
+cp support/standalone.xml $SERVER_CONF
+
+#echo " - limiting verbose JackRabbit logging in jboss-log4j.xml file..."
+#echo
+cp support/jboss-log4j.xml $SERVER_CONF
+
+# Add execute permissions to the standalone.sh script.
+echo " - making sure standalone.sh for server is executable..."
+echo
+chmod u+x $JBOSS_HOME/bin/standalone.sh
+
+echo " - enabling demo users for human tasks in jbpm-human-task.war web.xml file..."
+echo
+cp support/jbpm-human-task-war-web.xml $SERVER_DIR/jbpm-human-task.war/WEB-INF/web.xml
+
+echo " - enabling work items by registering Email and Log nodes..."
+echo
+cp support/drools.session.conf $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF
+cp support/CustomWorkItemHandlers.conf $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF
+chmod 644 $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF/drools.session.conf
+chmod 644 $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF/CustomWorkItemHandlers.conf
+
+echo " - adding rh6pd car inurance model jar to business central admin console classpath..."
+echo
+cp support/rh6pd-businessRulesModel.jar $SERVER_DIR/business-central-server.war/WEB-INF/lib
+
+echo " - adding rh6pd javascript war to servers deploy directory..."
+echo
+cp support/rh6pd-webJsInterface-1.0.0-SNAPSHOT.war $SERVER_DIR/rh6pd-webJsInterface-1.0.0-SNAPSHOT.war
+
+echo " - adding netty dep to business-central-server.war and jbpm-human-task.war..."
+echo
+cp support/MANIFEST.MF $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF/
+cp support/MANIFEST.MF $SERVER_DIR/jbpm-human-task.war/WEB-INF/classes/META-INF/
+
+echo " - install additional libraries for 6PD."
+echo
+cp support/lib/xstream-1.4.1.jar   $SERVER_DIR/business-central-server.war/WEB-INF/lib
+cp support/lib/xmlpull-1.1.3.1.jar $SERVER_DIR/business-central-server.war/WEB-INF/lib
+cp support/lib/xpp3_min-1.1.4c.jar $SERVER_DIR/business-central-server.war/WEB-INF/lib
+
+echo "JBoss BRMS ${DEMO} Setup Complete."
+echo
+
+
